743143f4d26edfd7800b378bb9620d916db95b12
aacfahim <aacfahim@gmail.com>
2022-07-11 19:33:20 +0600

    homepage money transfer icons

--- bkash_clone/lib/screens/home_page.dart
+++ bkash_clone/lib/screens/home_page.dart
@@ -9,6 +9,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Color AccentColor = Color(0xffE2136E);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,6 +77,208 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
+      ),
+      body: Column(
+        children: [
+          Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Container(
+              padding:
+                  const EdgeInsets.only(top: 10, left: 5, right: 5, bottom: 5),
+              height: MediaQuery.of(context).size.height * .25,
+              width: double.infinity,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Icon(Icons.send_to_mobile,
+                                size: 50, color: Color(0xffFBDDDE)),
+                            Text("Send Money"),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.mobile_friendly,
+                                size: 50, color: Color(0xff71A68A)),
+                            Text("Mobile Recharge"),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.money_off,
+                                size: 50, color: Color(0xff187F7B)),
+                            Text("Cash Out"),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.mobile_friendly,
+                                size: 50, color: Color(0xffB183BA)),
+                            Text("Make Payment"),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Icon(Icons.add, size: 50, color: Color(0xffFBDDDE)),
+                            Text("Add Money"),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.lightbulb,
+                                size: 50, color: Color(0xff5C7369)),
+                            Text("Pay Bill"),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.savings,
+                                size: 50, color: Color(0xffA65C90)),
+                            Text("Savings"),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.clean_hands,
+                                size: 50,
+                                color: Color.fromARGB(255, 225, 206, 171)),
+                            Text("Loan"),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Icon(Icons.money,
+                                size: 50, color: Color(0xffFBDDDE)),
+                            Text("Transfer Money"),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.cast_for_education,
+                                size: 50, color: Color(0xff71A68A)),
+                            Text("Education"),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.money_off,
+                                size: 35, color: Color(0xff187F7B)),
+                            Text("Remittance"),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.credit_card,
+                                size: 50, color: Color(0xff95C3B6)),
+                            Text("Loan"),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Icon(Icons.money,
+                                size: 50, color: Color(0xffFBDDDE)),
+                            Text("Transfer Money"),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.cast_for_education,
+                                size: 50, color: Color(0xff71A68A)),
+                            Text("Education"),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.money_off,
+                                size: 35, color: Color(0xff187F7B)),
+                            Text("Remittance"),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.credit_card,
+                                size: 50, color: Color(0xff95C3B6)),
+                            Text("Loan"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Card(
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("My bKash"),
+                  Text(
+                    "See All",
+                    style: TextStyle(color: AccentColor),
+                  ),
+                ],
+              ),
+              SingleChildScrollView(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [Icon(Icons.save_rounded), Text("Saved Bills")],
+                    ),
+                    Column(
+                      children: [Icon(Icons.favorite), Text("Priyo Numbers")],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.mobile_friendly),
+                        Text("018*******1")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.mobile_friendly),
+                        Text("017*******2")
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ]),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * .18,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/offer.jpg"),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(8),
+            ),
+          )
+        ],
       ),
     );
   }
